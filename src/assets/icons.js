110d52b578aec3bfcde82b2c457800b449bdb4c0
sri-rishi <sririshi2612@gmail.com>
2023-10-11 15:24:05 +0530

    chore: imported star icon from react-icons

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,6 +1,9 @@
 import { FaShoppingCart, } from "react-icons/fa";
-import { BiSolidUserCircle } from "react-icons/bi"
+import { BiSolidUserCircle } from "react-icons/bi";
+import { AiFillStar } from "react-icons/ai";
 
 export {
-    FaShoppingCart, BiSolidUserCircle
+    FaShoppingCart,
+    BiSolidUserCircle,
+    AiFillStar
 }
